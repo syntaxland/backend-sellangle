f48c4bc269dc73a7665ac8e13acc9333934036bd
syntaxland <chibuzo.okenwa@email.com>
2024-02-29 19:31:39 +0100

    new update

--- scripts/supervisor.sh
+++ scripts/supervisor.sh
@@ -12,6 +12,10 @@
 SUPERVISOR_CONFIG_DIR="/etc/supervisor/conf.d/"
 LOG_DIR="/home/ubuntu/backend-sellangle/logs/celery/"
 
+# Ensure log directories exist
+sudo mkdir -p "$LOG_DIR"
+sudo mkdir -p "$SUPERVISOR_CONFIG_DIR"
+
 # Reread and update Supervisor configuration
 sudo supervisorctl reread
 sudo supervisorctl update
@@ -21,11 +25,6 @@ sudo supervisorctl restart celerybeat
 
 # Start or restart Celery
 sudo supervisorctl restart celery 
-
-# Ensure log directories exist
-sudo mkdir -p "$LOG_DIR"
-
-sudo mkdir -p "$SUPERVISOR_CONFIG_DIR"
 
 # Check Supervisor status
 # sudo service supervisor status
