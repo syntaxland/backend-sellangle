51a554872ba9fb41510252c6d796e2713b90c8d1
syntaxland <chibuzo.okenwa@email.com>
2024-05-16 08:33:12 +0100

    new update

--- marketplace/urls.py
+++ marketplace/urls.py
@@ -3,17 +3,17 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('create-marketplace-seller/', views.create_marketplace_seller, name='create-marketplace-seller'),
-    path('marketplace-seller-photo/', views.create_marketplace_seller_photo, name='marketplace-seller-photo'), 
+    # path('create-marketplace-seller/', views.create_marketplace_seller, name='create-marketplace-seller'),
+    # path('marketplace-seller-photo/', views.create_marketplace_seller_photo, name='marketplace-seller-photo'), 
 
-    path('create-free-ad/', views.create_free_ad, name='create-free-ad'),
-    path('create-paid-ad/', views.create_paid_ad, name='create-paid-ad'),
+    # path('create-free-ad/', views.create_free_ad, name='create-free-ad'),
+    # path('create-paid-ad/', views.create_paid_ad, name='create-paid-ad'),
 
-    path('get-marketplace-seller-account/', views.get_marketplace_seller_account, name='get-marketplace-seller-account'),
-    path('update-marketplace-seller-account/', views.update_marketplace_seller_account, name='update-marketplace-seller-account'),
+    # path('get-marketplace-seller-account/', views.get_marketplace_seller_account, name='get-marketplace-seller-account'),
+    # path('update-marketplace-seller-account/', views.update_marketplace_seller_account, name='update-marketplace-seller-account'),
 
-    path('get-marketplace-seller-photo/', views.get_marketplace_seller_photo, name='get_marketplace-seller-photo'),
-    path('update-marketplace-seller-photo/', views.update_marketplace_seller_photo, name='update-marketplace-seller-photo'),
+    # path('get-marketplace-seller-photo/', views.get_marketplace_seller_photo, name='get_marketplace-seller-photo'),
+    # path('update-marketplace-seller-photo/', views.update_marketplace_seller_photo, name='update-marketplace-seller-photo'),
 
     # path('get-seller-free-ad/', views.get_seller_free_ad, name='get-seller-free-ad'),
     # path('get-seller-active-free-ads/<str:seller_username>/', views.get_seller_active_free_ads, name='get-seller-active-free-ads'),
