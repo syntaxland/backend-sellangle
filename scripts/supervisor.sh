51c990662043fe1cb49e99081a7b0c9099d51177
syntaxland <chibuzo.okenwa@email.com>
2024-06-16 11:19:32 +0100

    new update

--- scripts/supervisor.sh
+++ scripts/supervisor.sh
@@ -1,43 +1,50 @@
 #!/bin/bash
 
-# Check if Supervisor is installed
-if ! command -v supervisorctl &> /dev/null; then
-    echo "Supervisor is not installed. Installing Supervisor..."
-    sudo apt update
-    sudo apt install supervisor -y
+# Create log files if they don't exist
+sudo touch /home/ubuntu/backend-sellangle/logs/celery/worker-access.log 
+sudo touch /home/ubuntu/backend-sellangle/logs/celery/beat-access.log
 
-    # Check if installation was successful
-    if [ $? -ne 0 ]; then
-        echo "Failed to install Supervisor. Exiting."
-        exit 1
-    fi
+# Restart Supervisor to pick up changes
+sudo service supervisor restart
 
-    # Start Supervisor service
-    sudo service supervisor start
-fi
+# # Check if Supervisor is installed
+# if ! command -v supervisorctl &> /dev/null; then
+#     echo "Supervisor is not installed. Installing Supervisor..."
+#     sudo apt update
+#     sudo apt install supervisor -y
 
-# Check if Supervisor is running
-if ! sudo service supervisor status > /dev/null; then
-    echo "Supervisor is not running. Starting Supervisor..."
-    sudo service supervisor start
-fi
+#     # Check if installation was successful
+#     if [ $? -ne 0 ]; then
+#         echo "Failed to install Supervisor. Exiting."
+#         exit 1
+#     fi
 
-SUPERVISOR_CONFIG_DIR="/etc/supervisor/conf.d/"
-LOG_DIR="/home/ubuntu/backend-sellangle/logs/celery/"
+#     # Start Supervisor service
+#     sudo service supervisor start
+# fi
 
-# Ensure log directories exist
-sudo mkdir -p "$LOG_DIR"
-sudo mkdir -p "$SUPERVISOR_CONFIG_DIR"
+# # Check if Supervisor is running
+# if ! sudo service supervisor status > /dev/null; then
+#     echo "Supervisor is not running. Starting Supervisor..."
+#     sudo service supervisor start
+# fi
 
-# Reread and update Supervisor configuration
-sudo supervisorctl reread 
-sudo supervisorctl update
+# SUPERVISOR_CONFIG_DIR="/etc/supervisor/conf.d/"
+# LOG_DIR="/home/ubuntu/backend-sellangle/logs/celery/"
 
-# Start or restart Celerybeat
-sudo supervisorctl restart celerybeat 
+# # Ensure log directories exist
+# sudo mkdir -p "$LOG_DIR"
+# sudo mkdir -p "$SUPERVISOR_CONFIG_DIR"
 
-# Start or restart Celery
-sudo supervisorctl restart celery 
+# # Reread and update Supervisor configuration
+# sudo supervisorctl reread 
+# sudo supervisorctl update
 
-# Check Supervisor status after restarting services
-sudo service supervisor status
+# # Start or restart Celerybeat
+# sudo supervisorctl restart celerybeat 
+
+# # Start or restart Celery
+# sudo supervisorctl restart celery 
+
+# # Check Supervisor status after restarting services
+# sudo service supervisor status
