a331047963f1d064d0fe1accb6946af4bc93aedc
syntaxland <chibuzo.okenwa@email.com>
2024-06-17 07:04:44 +0100

    new update

--- scripts/supervisor.sh
+++ scripts/supervisor.sh
@@ -4,14 +4,14 @@
 sudo mkdir -p /etc/supervisor/conf.d/ 
  
 # Create Supervisor Log Directory
-sudo mkdir -p /var/log/supervisor/
+sudo mkdir -p /var/log/supervisor/ 
 
 # Create Celery Log Directory
 sudo mkdir -p /home/ubuntu/backend-sellangle/logs/celery/ 
 
 # Create log files if they don't exist
 sudo touch /home/ubuntu/backend-sellangle/logs/celery/worker-access.log 
-sudo touch /home/ubuntu/backend-sellangle/logs/celery/beat-access.log
+sudo touch /home/ubuntu/backend-sellangle/logs/celery/beat-access.log 
 
 # Restart Supervisor to pick up changes
 sudo service supervisor restart 
