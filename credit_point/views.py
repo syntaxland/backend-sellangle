87cea1a0b5243457574565f1d8c042e012aaf8b0
syntaxland <chibuzo.okenwa@email.com>
2024-07-03 19:06:45 +0100

    new update

--- credit_point/views.py
+++ credit_point/views.py
@@ -8,7 +8,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response 
 from rest_framework import status, generics
 from rest_framework.views import APIView  
-
+ 
 
 from .models import (CreditPoint,  
                     #  CreditPointRequest,
