1de3fd8a7c3bfe8b3bac8670c5b7358b8ddc7a53
softglobal <chibuzo.okenwa@gmail.com>
2024-10-02 09:28:25 +0100

    new update

--- backend_drf/settings.py
+++ backend_drf/settings.py
@@ -182,10 +182,10 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1), # testing
-    'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2), # testing
-    # 'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    # 'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
+    # 'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1), # testing
+    # 'REFRESH_TOKEN_LIFETIME': timedelta(minutes=2), # testing
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': True,
     'UPDATE_LAST_LOGIN': False,
